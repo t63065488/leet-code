7f8bd1cb71f5a600ca5e1342f9faac93878462e6
t63065488 <t63065488@gmail.com>
2023-12-21 16:03:41 +0000

    Refactor into unit tests

--- rust/src/merge_sorted_array.rs
+++ rust/src/merge_sorted_array.rs
@@ -1,6 +1,48 @@
-pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        nums1.truncate(m as usize);
-        nums2.truncate(n as usize);
-        nums1.append(nums2);
-        nums1.sort_unstable();
+pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) -> Vec<i32> {
+    nums1.truncate(m as usize);
+    nums2.truncate(n as usize);
+    nums1.append(nums2);
+    nums1.sort_unstable();
+    return nums1.to_owned();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_example1() {
+        // Given
+        let mut vec_a = vec![1, 2, 3, 0, 0, 0];
+        let mut vec_b = vec![2, 5, 6];
+        // When
+        let result = merge(&mut vec_a, 3, &mut vec_b, 3);
+        // Then
+        assert_eq!(6, result.len());
+        assert_eq!(vec![1, 2, 2, 3, 5, 6], result);
+    }
+
+    #[test]
+    fn test_merge_example2() {
+        // Given
+        let mut vec_a = vec![1];
+        let mut vec_b = vec![];
+        // When
+        let result = merge(&mut vec_a, 1, &mut vec_b, 0);
+        // Then
+        assert_eq!(1, result.len());
+        assert_eq!(vec![1], result);
+    }
+
+    #[test]
+    fn test_merge_example3() {
+        // Given
+        let mut vec_a = vec![0];
+        let mut vec_b = vec![1];
+        // When
+        let result = merge(&mut vec_a, 0, &mut vec_b, 1);
+        // Then
+        assert_eq!(1, result.len());
+        assert_eq!(vec![1], result);
+    }
 }
--- rust/src/remove_element.rs
+++ rust/src/remove_element.rs
@@ -11,3 +11,33 @@ pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
     }
     return nums.len() as i32;
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn remove_element_example1() {
+        // Given
+        let mut nums: Vec<i32> = vec![3, 2, 2, 3];
+        let val: i32 = 3;
+        // When
+        let result = remove_element(&mut nums, val);
+        // Then
+        assert_eq!(2, result);
+        assert_eq!(vec![2, 2], nums);
+    }
+
+    #[test]
+    fn remove_element_example2() {
+        // Given
+        let mut nums: Vec<i32> = vec![0, 1, 2, 2, 3, 0, 4, 2];
+        let val: i32 = 2;
+        // When
+        let result = remove_element(&mut nums, val);
+        // Then
+        assert_eq!(5, result);
+        assert_eq!(vec![0, 1, 0, 4, 3], nums);
+    }
+}
